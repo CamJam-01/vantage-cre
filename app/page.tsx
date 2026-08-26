e6a29a623d0b0c526a01c941ffee92e47e4ff1e4
CamJam <cjetton18@gmail.com>
2026-08-26 12:23:04 -0400

    record details and redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import { redirect } from 'next/navigation';
+<<<<<<< HEAD
 
 export default function HomePage() {
+=======
+import { createClient } from '@/lib/supabase/server';
+
+export default async function Home() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect('/search');
+  }
+
+>>>>>>> 0c18f7c (add redirect for root domain for non-logged in users + changed placeholder text in sales search)
   redirect('/login');
 }
